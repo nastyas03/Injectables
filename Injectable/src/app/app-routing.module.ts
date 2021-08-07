817a9a80274869d514e9a2170180145214f37c96
YasTab <nastyas03@hotmail.com>
2021-08-07 13:30:51 +0200

    Add routing

--- Injectable/src/app/app-routing.module.ts
+++ Injectable/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path : 'home', component : SuperHeroesListComponent}
+  { path : '', component : SuperHeroesListComponent}
 ];
 
 @NgModule({
